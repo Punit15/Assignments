b168e956f550b3b52212af96e50f7dfc5d813057
Punit15 <punit3529@gmail.com>
2017-02-08 13:08:16 +0530

    fist proj

---
+++ src/assignments/firstassignment.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignments;
+
+/**
+ *
+ * @author punit
+ */
+public class firstassignment {
+    
+    public static void main(String[] args) {
+        System.out.println("assignments.firstassignment.main()");
+    }
+}
